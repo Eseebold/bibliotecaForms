d313e988800d3c71d0379447418e4ba930933594
TriquiTrueno <erasmo.seebold@gmail.com>
2016-10-06 17:58:28 +0200

    Creacion de clase usuario

---
+++ bibliotecaForms/DAL/UsuarioRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using bibliotecaForms.Models;
+
+namespace bibliotecaForms.DAL
+{
+    interface UsuarioRepository
+    {
+      
+            IList<Usuario> getAll();
+            Usuario getById(Guid codigo);
+            Usuario update(Usuario usuario)
+        
+    }
+}
---
+++ bibliotecaForms/DAL/UsuarioRepositoryImp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using bibliotecaForms.Models;
+
+namespace bibliotecaForms.DAL
+{
+    public class UsuarioRepositoryImp : UsuarioRepository
+    {
+        public IList<Usuario> getAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario getById(Guid codigo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario update(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
